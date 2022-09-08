22505dd8c797b23938e91afb44aba24a7c630c86
tysonscamahorn <105517106+tysonscamahorn@users.noreply.github.com>
2022-09-08 16:02:30 -0600

    third commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿int a = 18;
-int b = 6;
-int c = a + b;
-Console.WriteLine(c);
-c = a - b;
-Console.WriteLine(c);
-c = a * b;
-Console.WriteLine(c);
-c = a / b;
-Console.WriteLine(c);
+﻿WorkWithIntegers();
+
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    c = a * b;
+    Console.WriteLine(c);
+    c = a/ b;
+    Console.WriteLine(c);
+
+}

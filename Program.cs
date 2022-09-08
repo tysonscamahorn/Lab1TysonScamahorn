e9921ed3be0763de8ac157456be5bc13d1f0aeb3
tysonscamahorn <105517106+tysonscamahorn@users.noreply.github.com>
2022-09-08 16:23:28 -0600

    seventh commit

--- Program.cs
+++ Program.cs
@@ -1,43 +1,21 @@
-﻿WorkWithIntegers();
-OrderPrecedence();
+﻿double a = 5;
+double b = 4;
+double c = 2;
+double d = (a + b) / c;
+Console.WriteLine(d);
 
-void WorkWithIntegers()
-{
-    int a = 18;
-    int b = 6;
-    int c = a + b;
-    Console.WriteLine(c);
-    c = a - b;
-    Console.WriteLine(c);
-    c = a * b;
-    Console.WriteLine(c);
-    c = a / b;
-    Console.WriteLine(c);
-}
+double e = 19;
+double f = 23;
+double g = 8;
+double h = (e + f) / g;
+Console.WriteLine(h);
 
-void OrderPrecedence()
-{
-    int a = 5;
-    int b = 4;
-    int c = 2;
-    int d = a + b * c;
-    Console.WriteLine(d);
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
 
-    d = (a + b) * c;
-    Console.WriteLine(d);
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
 
-    d = (a + b) - 6 * c + (12 * 4) % 3 + 12;
-    Console.WriteLine(d);
 
-    int e = 7;
-    int f = 4;
-    int g = 3;
-    int h = (e + f) % g;
-    Console.WriteLine($"quotient: {h}");
-}
 
-int max = int.MaxValue;
-int min = int.MinValue;
-Console.WriteLine($"The range of integers is {min} to {max}");
-int what = max + 3;
-Console.WriteLine($"Example of an overflow: {what}");

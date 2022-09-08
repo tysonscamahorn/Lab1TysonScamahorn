772eaf0a4b777ec3fd6dc82d576ada4852402815
tysonscamahorn <105517106+tysonscamahorn@users.noreply.github.com>
2022-09-08 15:48:00 -0600

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+c = a - b;
+Console.WriteLine(c);
+c = a * b;
+Console.WriteLine(c);
+c = a / b;
+Console.WriteLine(c);

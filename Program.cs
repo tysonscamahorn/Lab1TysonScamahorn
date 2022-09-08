ce0105abe23ad2624ac78331cd20846fa0b1ce50
tysonscamahorn <105517106+tysonscamahorn@users.noreply.github.com>
2022-09-08 16:10:55 -0600

    fith commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿int a = 5;
-int b = 4;
-int c = 2;
-int d = a + b * c;
-Console.WriteLine(d);
-d = (a + b) * c;
-Console.WriteLine(d);
-d = (a-c)^2+4-b*2;
-Console.WriteLine(d);
+﻿//WorkWithIntegers();
+OrderPrecedence();
 
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+    c = a - b;
+    Console.WriteLine(c);
+    c = a * b;
+    Console.WriteLine(c);
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
+}

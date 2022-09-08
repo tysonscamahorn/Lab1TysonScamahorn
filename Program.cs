8341e10e930c5975474b485f3fc0856f0f605411
tysonscamahorn <105517106+tysonscamahorn@users.noreply.github.com>
2022-09-08 16:41:49 -0600

    final commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿double a = 5;
-double b = 4;
+﻿
+
+double a = 1.0;
+double b = 3.0;
 double c = 2;
 double d = (a + b) / c;
 Console.WriteLine(d);
@@ -17,5 +19,14 @@ Console.WriteLine($"The range of double is {min} to {max}");
 double third = 1.0 / 3.0;
 Console.WriteLine(third);
 
+Console.WriteLine(a / b);
 
+decimal one = 1.0M;
+decimal two = 3.0M;
+Console.WriteLine(one / two);
 
+double pie = Math.PI;
+double r = 2.5;
+double circle =pie * r  * r;
+
+Console.WriteLine(circle);
